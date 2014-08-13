b84e07d9137012ab47c8ead571bab5f50e3707b4
Meredith Howard <mhoward@roomag.org>
2014-08-13 10:06:49 -0400

    Update, got omz files in right place

--- .homedir-setup.rb
+++ .homedir-setup.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # vim: et sts=2 sw=2 fdm=marker
 
-# we don't yet have rbenv so keep safe for ruby 1.8
+# we don't yet have rbenv so keep safe for ruby 1.9
 
 if `which git` == ''
   puts "please install git"
@@ -9,7 +9,7 @@ if `which git` == ''
 end
 
 
-# just checks and bails after each call
+# just checks and bails after each call, output already in STDERR
 def do_cmds (*cmds)
   cmds.each do |cmd|
     puts "$ #{cmd}"
@@ -20,9 +20,7 @@ end
 
 
 # oh-my-zsh {{{
-  # i have a custom theme and the omz install complains about already
-  # having a directory there.  remove, install, then revert my stuff
-  # out of hg
+  # some custom zsh files are already in .oh-my-zsh.cust
 
   if Dir.exists?('.oh-my-zsh/.git')
     puts "oh-my-zsh already installed"
@@ -30,8 +28,7 @@ end
     puts "Installing oh-my-zsh..."
 
     do_cmds \
-      'git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh',
-      'hg stat -dn0 -I .oh-my-zsh/ | xargs -0 hg revert'
+      'git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh'
 
   end
 
@@ -63,7 +60,7 @@ end
     do_cmds \
       'git clone https://github.com/sstephenson/rbenv.git .rbenv',
       'git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins/ruby-build/'
-    
+
   end
 # }}}
 
