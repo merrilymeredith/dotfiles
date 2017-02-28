c153413b0df10baf095fb197186e9621a87cb220
Meredith Howard <mhoward@roomag.org>
2017-02-28 03:04:07 -0500

    Get some vimperator stuff too

--- .vim/windows-vimfiles.sh
+++ .vim/windows-vimfiles.sh
@@ -19,11 +19,18 @@ fi
 
 cp -vuRL -t ${USERPROFILE}/vimfiles .vim/*
 
-# rebuilding vimproc dll on babun for w32 gvim:
-# $ pact install mingw-gcc-core
-# $ make -f make_mingw32.mak CC=i686-pc-mingw32-gcc
+cp -vu .vimperatorrc ${USERPROFILE}/
 
-# for w64 gvim:
-# $ pact install mingw64-x86_64-gcc-core
-# $ make -f make_mingw64.mak CC=x86_64-w64-mingw32-gcc
+mkdir ${USERPROFILE}/{css,js} &>/dev/null || true
+cp -vuRL -t ${USERPROFILE}/css .css/*
+cp -vuRL -t ${USERPROFILE}/js .js/*
 
+cat <<ENDINSTR
+#  rebuilding vimproc dll on babun for w32 gvim:
+#  $ pact install mingw-gcc-core
+#  $ make -f make_mingw32.mak CC=i686-pc-mingw32-gcc
+
+#  for w64 gvim:
+#  $ pact install mingw64-x86_64-gcc-core
+#  $ make -f make_mingw64.mak CC=x86_64-w64-mingw32-gcc
+ENDINSTR
