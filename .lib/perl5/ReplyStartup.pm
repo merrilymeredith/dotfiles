698fd7dea935c5f7b712a6654c2c05a8cbb5f882
Meredith Howard <mhoward@roomag.org>
2018-02-15 15:29:28 -0500

    move reply startup

---
+++ .lib/perl5/ReplyStartup.pm
@@ -0,0 +1,22 @@
+package ReplyStartup;
+
+sub import {
+  my $code = eval(<<END_PERL) or die $@;
+    package main;
+    sub {
+      warnings->import;
+      strict->import;
+      require feature; feature->import(':5.14');
+      require experimentals; experimentals->import;
+
+      use Path::Tiny;
+
+      -e \$_ && do(\$_) for "\$ENV{HOME}/.replyrc.local.pl";
+      die \$@ if \$@;
+    }
+END_PERL
+
+  $code->();
+}
+
+1;
