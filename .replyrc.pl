d781bcc444d4bb0ce4d153aa138f140221cf5257
Meredith Howard <mhoward@roomag.org>
2018-03-14 11:18:34 -0700

    add ddp config toggles

--- .replyrc.pl
+++ .replyrc.pl
@@ -7,4 +7,22 @@ use Data::Printer {
 
 use Path::Tiny;
 
+
+# This is extra awkward bc Data::Printer has no introspection of its config.
+# These values need to mirror what's set above, or defaults.
+my $ddp_show_private   = 0;
+my $ddp_show_inherited = 1;
+
+sub ddp_toggle_private {
+  my $val = ($ddp_show_private ^= 1) ? 'all' : 'public';
+  Data::Printer->import({class => {show_methods => $val}});
+  $val;
+}
+
+sub ddp_toggle_inherited {
+  my $val = ($ddp_show_inherited ^= 1) ? 'public' : 'none';
+  Data::Printer->import({class => {inherited => $val}});
+  $val;
+}
+
 1;
