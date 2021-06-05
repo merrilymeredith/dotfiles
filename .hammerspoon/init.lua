bc0c937c21250bc0ba4d647241410bac6d77cdde
Meredith Howard <mhoward@roomag.org>
2021-06-04 23:35:16 -0500

    Fix up focus-to shortcuts for new hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,23 +6,31 @@ local cmd      = hs.fnutils.partial(bindKey, {"cmd"})
 local cmdShift = hs.fnutils.partial(bindKey, {"cmd", "shift"})
 local cmdCtrl  = hs.fnutils.partial(bindKey, {"cmd", "ctrl"})
 
-function focusedWindow() return hs.window.focusedWindow() or hs.window.desktop() end
-function focusTo(direction) hs.window['focusWindow' .. direction](focusedWindow()) end
+function focusedWindow()
+  return hs.window.focusedWindow() or hs.window.desktop()
+end
+
+function focusTo(direction)
+  local fn = hs.getObjectMetatable('hs.window')['focusWindow' .. direction]
+  return function ()
+    fn(focusedWindow(), nil, nil, true)
+  end
+end
 
 -- and the key bindings
 cmdShift('r', hs.reload)
 
-cmd('k',  function () focusTo('North') end)
-cmd('up', function () focusTo('North') end)
+cmd('k',  focusTo('North'))
+cmd('up', focusTo('North'))
 
-cmd('j',    function () focusTo('South') end)
-cmd('down', function () focusTo('South') end)
+cmd('j',    focusTo('South'))
+cmd('down', focusTo('South'))
 
-cmd('h',    function () focusTo('West') end)
-cmd('left', function () focusTo('West') end)
+cmd('h',    focusTo('West'))
+cmd('left', focusTo('West'))
 
-cmd('l',     function () focusTo('East') end)
-cmd('right', function () focusTo('East') end)
+cmd('l',     focusTo('East'))
+cmd('right', focusTo('East'))
 
 cmdShift('up', function () focusedWindow():maximize() end)
 
