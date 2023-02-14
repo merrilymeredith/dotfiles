843fe817e6d04f452ad7bf1df38d605f553b6eab
Meredith Howard <mhoward@roomag.org>
2023-02-14 02:39:04 -0600

    Add more plain vim plugins

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -1,4 +1,12 @@
 return {
-  "tpope/vim-vinegar",
+  "editorconfig/editorconfig-vim",
   "tpope/vim-unimpaired",
+  "godlygeek/tabular",
+  "tomtom/tcomment_vim",
+  "tpope/vim-endwise",
+  {"mbbill/undotree", cmd = "UndotreeToggle"},
+
+  "tpope/vim-vinegar",
+  "kshenoy/vim-signature",
+  {"majutsushi/tagbar", cmd = "TagbarToggle"},
 }
