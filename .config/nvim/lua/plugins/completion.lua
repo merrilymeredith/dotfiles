b745f2970390f966c554db5d4f32f364fe6ffdca
Meredith Howard <mhoward@roomag.org>
2023-02-27 13:49:07 -0600

    tweak completion a little more

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -16,7 +16,16 @@ return {
       local cmp = require("cmp")
       return {
         completion = {
+          keyword_length = 3,
           completeopt = "menu,menuone,noinsert,noselect",
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            if vim_item.kind == "Text" then
+              vim_item.kind = entry.source.name
+            end
+            return vim_item
+          end,
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -38,13 +47,11 @@ return {
           { name = "nvim_lsp" },
           { name = "nvim_lsp_signature_help" },
           { name = "nvim_lua" },
-        }, {
-          { name = "calc" },
-          { name = "buffer" },
+          { name = "buffer", option = { keyword_pattern = [[\k\+]] } },
           { name = "path" },
-        }, {
-          { name = "tags" },
           { name = "omni" },
+          { name = "tags" },
+          { name = "calc" },
         }),
       }
     end,
