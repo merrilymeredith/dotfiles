6cf2406257a869821532dea718c33a0bb324f3dd
Meredith Howard <mhoward@roomag.org>
2023-03-01 03:31:48 -0500

    Add treesitter for elixir support

--- .config/nvim/lua/plugins/filetype.lua
+++ .config/nvim/lua/plugins/filetype.lua
@@ -7,6 +7,24 @@ g.perl_sync_dist = 300
 g.perl_compiler_force_warnings = 0
 
 return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = function()
+      require('nvim-treesitter.install').update({ with_sync = true })
+    end,
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          'eex',
+          'elixir',
+          'heex',
+          'surface',
+        },
+      })
+    end,
+  },
   'Shougo/vinarise.vim',
   'asciidoc/vim-asciidoc',
   {'vim-perl/vim-perl', branch = 'dev'},
