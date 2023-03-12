cfe2011415676ce0501a3b3ef7a23f5618bef8fa
Meredith Howard <mhoward@roomag.org>
2023-03-12 03:18:04 -0500

    unlazy indent-blankline.nvim, weird errors

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -35,7 +35,7 @@ return {
 
   {
     "lukas-reineke/indent-blankline.nvim",
-    event = { "BufReadPost", "BufNewFile" },
+    lazy = false,
     keys = {
       { "<leader>ig", "<cmd>IndentBlanklineToggle<cr>" },
     },
