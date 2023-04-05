c4ff0944d6f92125f09e370596f8c308ee43703e
Meredith Howard <mhoward@roomag.org>
2023-04-05 01:26:04 -0500

    actually set up lsp snippets

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -10,14 +10,27 @@ return {
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-omni",
       "hrsh7th/cmp-path",
+      { "L3MON4D3/LuaSnip", opts = { history = true, delete_check_events = "TextChanged" } },
       "quangnguyen30192/cmp-nvim-tags",
     },
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       cmp.setup({
         preselect = cmp.PreselectMode.None,
         completion = {
           keyword_length = 3,
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
         formatting = {
           format = function(entry, vim_item)
@@ -32,15 +45,36 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
           ["<C-g>"] = cmp.mapping.abort(),
           ["<Right>"] = cmp.mapping.confirm({ select = true }),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<Space>"] = function(fallback)
-            if cmp.visible() then
+            local e = cmp.get_active_entry()
+            if cmp.visible() and e then
               cmp.confirm({ select = false }, function()
-                vim.api.nvim_feedkeys(" ", "n", false)
+                if e:get_kind() ~= cmp.lsp.CompletionItemKind.Snippet then
+                  vim.api.nvim_feedkeys(" ", "n", false)
+                end
               end)
             end
             fallback()
