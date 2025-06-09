5b748721337d2633b05a98f13d6fdec919700b8e
Meredith Howard <mhoward@roomag.org>
2025-06-09 01:51:25 -0500

    add venv wrapper

---
+++ .lib/sh/venv_wrapper.sh
@@ -0,0 +1,30 @@
+venv() {
+  local venv_home="${VENV_HOME:-$HOME/.venvs}"
+  local venv="${1:?Please provide venv name}"
+
+  source "$venv_home/$venv/bin/activate"
+}
+
+lsvenv() {
+  local venv_home="${VENV_HOME:-$HOME/.venvs}"
+  ls -1 "$venv_home"
+}
+
+mkvenv() {
+  local venv_home="${VENV_HOME:-$HOME/.venvs}"
+  local venv="${1:?Please provide venv name}"
+
+  mkdir -p "$venv_home"
+  python3 -m venv "$venv_home/$venv"
+}
+
+rmvenv() {
+  local venv_home="${VENV_HOME:-$HOME/.venvs}"
+  local venv="${1:?Please provide venv name}"
+
+  if [[ ! -d "$venv_home/$venv" ]]; then
+    echo "venv $venv does not exist" >&2
+  fi
+
+  rm -rf "$venv_home/$venv"
+}
