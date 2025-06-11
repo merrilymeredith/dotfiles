975a594c35260ac4ecddf746efe4cf4573f2f15f
Meredith Howard <mhoward@roomag.org>
2025-06-10 22:01:31 -0500

    Simplify down to virtual_lines / current_line

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,32 +1,15 @@
 vim.diagnostic.config({
-  float = {
-    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-    focusable = false,
-    header = "",
-    max_width = 55,
-    source = "if_many",
-    severity = { max = vim.diagnostic.severity.WARN },
-  },
   severity_sort = true,
-  virtual_lines = {
-    current_line = true,
-    severity = vim.diagnostic.severity.ERROR
-  },
+  virtual_lines = { current_line = true },
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
   once = true,
   group = "lsp_attach",
-  callback = function(args)
+  callback = function()
     -- Some options are more chill in text mode, this unchills them if a LSP is
-    -- in play.  Note they're global and this is a non-once aucmd
+    -- in play.  Note they're global and this is a once aucmd
     vim.opt.number = true
     vim.opt.updatetime = 250
-
-    vim.api.nvim_create_autocmd("CursorHold", {
-      group = vim.api.nvim_create_augroup("lsp_buf_diags", { clear = true }),
-      buffer = args.buf,
-      callback = function(_) vim.diagnostic.open_float() end,
-    })
   end,
 })
