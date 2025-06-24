d7149049e74de41b4224515b25e883c01b4c337c
Meredith Howard <mhoward@roomag.org>
2025-06-24 18:03:06 -0400

    fix auto-prune: backup files keep original mtime, use atime

--- .config/nvim/plugin/auto-prune.lua
+++ .config/nvim/plugin/auto-prune.lua
@@ -2,15 +2,16 @@
 
 local fn, fs = vim.fn, vim.fs
 
-local function prune_files(path, days)
+local function prune_files(path, days, which_time)
   local sunset = os.time() - (days * 86400)
   path = fs.normalize(path)
+  which_time = which_time or "mtime"
 
   if fn.getftype(path) == "" then return end
 
   for fname, type in fs.dir(path) do
     local fpath = fs.normalize(path .. "/" .. fname)
-    if type == "file" and fn.getftime(fpath) < sunset then
+    if type == "file" and vim.uv.fs_stat(fpath)[which_time].sec < sunset then
       os.remove(fpath)
     end
   end
@@ -18,7 +19,7 @@ end
 
 local function auto_prune()
     if vim.go.swapfile then prune_files(vim.go.directory, 90) end
-    if vim.go.backup then prune_files(vim.go.backupdir, 90) end
+    if vim.go.backup then prune_files(vim.go.backupdir, 90, "atime") end
     if vim.go.undofile then prune_files(vim.go.undodir, 90) end
 end
 
