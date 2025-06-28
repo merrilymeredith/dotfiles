2e86f7e6541fe29e590e5d3c057467e38c182a31
Meredith Howard <mhoward@roomag.org>
2025-06-28 00:32:36 -0500

    Customize molten-nvim more

--- .config/nvim/lua/plugins/notebook.lua
+++ .config/nvim/lua/plugins/notebook.lua
@@ -21,16 +21,25 @@ return {
   },
   {
     "benlubas/molten-nvim",
+    branch = "main",
     cond = (vim.fn.has("python3") == 1),
     build = ":UpdateRemotePlugins",
     keys = {
       { "<leader>me", ":MoltenEvaluateOperator<CR>", desc = "eval operator" },
       { "<leader>me", ":<C-u>MoltenEvaluateVisual<CR>", desc = "eval selection", mode = "v" },
+      { "<leader>ml", ":MoltenEvaluateLine<CR>", desc = "eval line" },
       { "<leader>mr", ":MoltenReevaluateCell<CR>", desc = "re-eval cell" },
       { "<leader>md", ":MoltenDelete<CR>", desc = "forget cell" },
-      { "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", desc = "open output window" },
-      { "<leader>mh", ":MoltenHideOutput<CR>", desc = "hide output window"},
+      { "<leader>mD", ":MoltenDelete!<CR>", desc = "forget all cells" },
+      { "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", desc = "open/enter output window" },
+      { "<leader>mh", ":MoltenToggleVirtual!<CR>", desc = "toggle output virtual text"},
       { "<leader>mx", ":MoltenOpenInBrowser<CR>", desc = "open output in browser"},
+      { "[c", ":MoltenPrev<CR>", desc = "previous code cell" },
+      { "]c", ":MoltenNext<CR>", desc = "next code cell" },
     },
+    config = function()
+      vim.api.nvim_set_hl(0, "MoltenCell", {link = "DiffChange"})
+      vim.api.nvim_set_hl(0, "MoltenVirtualText", {link = "MoreMsg"})
+    end
   },
 }
