02336d77663c5489c8e91d0428bda5345f66d39f
Meredith Howard <mhoward@roomag.org>
2025-07-09 21:37:31 -0500

    add wezterm.lua

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,55 @@
+local wezterm = require 'wezterm'
+local c = wezterm.config_builder()
+
+c.font = wezterm.font_with_fallback {
+  'DejaVu Sans Mono',
+  'Cascadia Code',
+  'Consolas',
+}
+c.font_size = 10.0
+c.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+c.freetype_load_target = "Light"
+
+c.animation_fps = 1
+c.cursor_blink_ease_in = 'Constant'
+c.cursor_blink_ease_out = 'Constant'
+
+c.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+c.window_padding = { left = 7, right = 7, top = 7, bottom = 7 }
+c.initial_cols, c.initial_rows = 120, 35
+
+c.launch_menu = {}
+
+if wezterm.target_triple:match("windows") then
+  -- c.default_prog = { 'pwsh.exe' }
+  -- So WSL always breaks window focus on start, both windows terminal and wezterm
+  c.default_prog = { 'wsl.exe', '--cd', '~' }
+
+  table.insert(c.launch_menu, {
+    label = 'pwsh',
+    args = { 'pwsh.exe' },
+  })
+
+  table.insert(c.launch_menu, {
+    label = 'cmd',
+    args = { 'cmd.exe' },
+  })
+
+  table.insert(c.launch_menu, {
+    label = "wsl nvim",
+    args = { "wsl.exe", "--cd", "~", "nvim" }
+  })
+
+  -- on Intel Iris Xe graphics, there's artifacting unless you use `prefer_egl`
+  -- or `front_end = "WebGpu"`.  WebGpu renders fonts too heavy though.
+  -- Also, I think prefer_egl is the -intended- default?
+  c.prefer_egl = true
+end
+
+local ok, wezterm_local = pcall(require, "wezterm_local")
+if ok then
+  wezterm_local.apply(c)
+end
+
+return c
